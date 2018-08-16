9b993d7a7ffbb44946c94c11709608d7aa535afd
KyleDinnison <kingkyle01@hotmail.com>
2018-08-16 14:47:42 +1000

    Refactored password program

--- Password_entry.py
+++ Password_entry.py
@@ -1,9 +1,18 @@
 '''Kyle'''
-password = str(input('Password: '))
-while len(password) < 5 or len(password) > 15:
-    print('Invalid password')
+
+
+def main():
+    password = get_password()
+
+    print('*' * len(password))
+
+
+def get_password():
     password = str(input('Password: '))
+    while len(password) < 5 or len(password) > 15:
+        print('Invalid password')
+        password = str(input('Password: '))
+    return password
 
-print('*' * len(password))
 
-
+main()
