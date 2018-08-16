853824723c0e44eb0c8bbf0e667c6426ce0633b4
KyleDinnison <kingkyle01@hotmail.com>
2018-08-16 14:19:25 +1000

    Added password checker

---
+++ Sandbox.py
@@ -0,0 +1,6 @@
+password = str(input('Password: '))
+while len(password) < 5 or len(password) > 15:
+    print('Invalid password')
+    password = str(input('Password: '))
+
+print('*' * len(password))
